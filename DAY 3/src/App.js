0c65bc0bce86cec75140a7f4730e573353a316e5
kavya9363 <110531961+kavya9363@users.noreply.github.com>
2023-09-15 10:52:07 +0530

    Add files via upload

---
+++ DAY 3/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './pages/login';
+// import Signup from './pages/signup';
+function App() {
+  return (
+    <div>
+      {/* <Signup/> */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
