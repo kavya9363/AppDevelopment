077e67bb004f1e4e0eac8f9cef992b6501617f6a
kavya9363 <110531961+kavya9363@users.noreply.github.com>
2023-09-22 13:03:15 +0530

    Add files via upload

---
+++ Day 7/src/pages/Home/Home.js
@@ -0,0 +1,34 @@
+// import React from "react";
+// import BannerBackground from "../../assets/img/home-banner-background.png";
+// import BannerImage from "../../assets/img/home-banner-image.png";
+// import Navbar from "./Navbar";
+// import { FiArrowRight } from "react-icons/fi";
+
+// const Home = () => {
+//   return (
+//     <div className="home-container">
+//       <Navbar />
+//       <div className="home-banner-container">
+//         <div className="home-bannerImage-container">
+//           <img src={BannerBackground} alt="" />
+//         </div>
+//         <div className="home-text-section">
+//           <h1 className="primary-heading">
+//           Innovative learning platform
+//           </h1>
+//           <p className="primary-text">
+//           "A journey of a thousand miles begins with a single step."—Sun Tzu.
+//           </p>
+//           <button className="secondary-button">
+//             Book slot <FiArrowRight />{" "}
+//           </button>
+//         </div>
+//         <div className="home-image-section">
+//           <img src={BannerImage} alt="" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
